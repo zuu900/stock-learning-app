903cc26c374b6f9838eb7d91e29d267059eb2293
watanabe <k.watanabe0709@keio.jp>
2025-07-10 15:47:47 +0900

    feat: Integrate database and display mock data

--- src/app.ts
+++ src/app.ts
@@ -17,24 +17,6 @@ app.get("/api/lessons", async (req: Request, res: Response) => {
   }
 });
 
-app.get("/api/lessons/:id", async (req: Request, res: Response) => {
-  const { id } = req.params;
-  try {
-    const lesson = await prisma.stockLesson.findUnique({
-      where: { id: Number(id) }, // URLのidを使って一件だけ検索
-    });
-
-    if (!lesson) {
-      return res.status(404).json({ error: "Lesson not found" });
-    }
-
-    res.json(lesson);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Failed to fetch lesson" });
-  }
-});
-
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
